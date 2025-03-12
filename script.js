983667b886764ee8d0ae86beb1eaef38586f268b
shauryatripathi <shauryatripathi50@gmail.com>
2025-03-12 13:13:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 window.showMessage = function () {
     console.log('Hello! This is a message from JavaScript.');
     alert('Hello! This is a message from JavaScript.');
-}
+};
+
+function showHello() {
+    console.log('Hello! This is a message from JavaScript.');
+    alert('Hello! This is a message from JavaScript.');
+};
